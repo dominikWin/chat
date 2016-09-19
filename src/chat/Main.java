fb6918f701f4ce542372591a9f32ba76123a082b
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-19 19:41:15 -0400

    Add stdin hash test

--- src/chat/Main.java
+++ src/chat/Main.java
@@ -1,8 +1,10 @@
 package chat;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		ChatContent c = new ChatContent("text");
+		ChatContent c = new ChatContent(new Scanner(System.in).nextLine());
 		
 		ChatSHA1 h = new  ChatSHA1(c);
 		System.out.println(h);
