9446bf2f55d1392af6784e7c6d528054cb267f69
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-20 17:22:12 -0400

    Update database methods

--- src/chat/LocalDB.java
+++ src/chat/LocalDB.java
@@ -5,6 +5,7 @@ import java.io.File;
 import chat.data.ChatContent;
 import chat.data.ChatNode;
 import chat.interfaces.ChatDatabase;
+import chat.interfaces.ChatHash;
 
 public class LocalDB implements ChatDatabase {
 	
@@ -12,19 +13,23 @@ public class LocalDB implements ChatDatabase {
 	private static final String DIR_NAME = ".chatdata";
 	private static final String OBJECT_DIR_NAME = "objects";
 	
-	private boolean dataDirExists() {
-		File f = new File(PATH + DIR_NAME);
+	private boolean dirExists(String path) {
+		File f = new File(path);
 		return f.exists() && f.isDirectory();
+	}
+	
+	private boolean dataDirExists() {
+		return dirExists(PATH + DIR_NAME);
 	}
 
 	@Override
-	public boolean existsNode(String hash) {
+	public boolean existsNode(ChatHash hash) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean existsContent(String hash) {
+	public boolean existsContent(ChatHash hash) {
 		// TODO Auto-generated method stub
 		return false;
 	}
--- src/chat/interfaces/ChatDatabase.java
+++ src/chat/interfaces/ChatDatabase.java
@@ -4,8 +4,8 @@ import chat.data.ChatContent;
 import chat.data.ChatNode;
 
 public interface ChatDatabase {
-	public boolean existsNode(String hash);
-	public boolean existsContent(String hash);
+	public boolean existsNode(ChatHash hash);
+	public boolean existsContent(ChatHash hash);
 	
 	public void add(ChatNode node);
 	public void add(ChatContent content);
