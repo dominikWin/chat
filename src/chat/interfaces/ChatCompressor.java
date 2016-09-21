acde77103fb48f7748a9075709d91c0299085e40
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 19:37:57 -0400

    Add doc to ChatCompressor

--- src/chat/interfaces/ChatCompressor.java
+++ src/chat/interfaces/ChatCompressor.java
@@ -1,9 +1,20 @@
 package chat.interfaces;
 
-import chat.data.ChatText;
-import chat.data.ChatNode;
-
+/**
+ * Interface that allows for compression and decompression of data.
+ * All data uses {@code byte[]}s.
+ */
 public interface ChatCompressor {
+	
+	/**
+	 * @param data
+	 * @return a compressed representation of {@code data}.
+	 */
 	public byte[] deflate(byte[] data);
+	
+	/**
+	 * @param data
+	 * @return a decompressed representation of {@code data}.
+	 */
 	public byte[] inflate(byte[] data);
 }
