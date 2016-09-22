e5ff8586eeb4e3a56cd3231d59a7068ebc111aa1
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:18:49 -0400

    Add doc to CompressZLIB

--- src/chat/CompressZLIB.java
+++ src/chat/CompressZLIB.java
@@ -3,12 +3,13 @@ package chat;
 import chat.interfaces.ChatCompressor;
 import java.util.zip.*;
 
-public class CompressZLIB implements ChatCompressor
-{
+/**
+ * ZLIB implementation of {@link ChatCompressor}.
+ */
+public class CompressZLIB implements ChatCompressor {
 
 	@Override
-	public byte[] deflate(byte[] data)
-	{
+	public byte[] deflate(byte[] data) {
 		Deflater deflater = new Deflater();
 		deflater.setInput(data);
 		deflater.finish();
@@ -20,24 +21,21 @@ public class CompressZLIB implements ChatCompressor
 		if (writen >= 2048)
 			System.err.println("To large compressed message");
 		byte out[] = new byte[writen];
-		for(int i = 0; i < writen; i++)
+		for (int i = 0; i < writen; i++)
 			out[i] = buffer[i];
 		return out;
 	}
 
 	@Override
-	public byte[] inflate(byte[] data)
-	{
+	public byte[] inflate(byte[] data) {
 		Inflater inflater = new Inflater();
 		inflater.setInput(data);
 
 		int writen = 0;
 		byte buffer[] = new byte[2048];
-		try
-		{
+		try {
 			writen = inflater.inflate(buffer);
-		} catch (DataFormatException e)
-		{
+		} catch (DataFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -45,7 +43,7 @@ public class CompressZLIB implements ChatCompressor
 		if (writen >= 2048)
 			System.err.println("To large decompressed message");
 		byte out[] = new byte[writen];
-		for(int i = 0; i < writen; i++)
+		for (int i = 0; i < writen; i++)
 			out[i] = buffer[i];
 		return out;
 	}
