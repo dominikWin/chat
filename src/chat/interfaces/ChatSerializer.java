88f6e47f2341036fdaa3f2fe7d14e1325e4c5af8
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:07:15 -0400

    Add doc to ChatSerializer

--- src/chat/interfaces/ChatSerializer.java
+++ src/chat/interfaces/ChatSerializer.java
@@ -3,9 +3,27 @@ package chat.interfaces;
 import chat.data.ChatText;
 import chat.data.ChatNode;
 
+/**
+ * Interface to serialize {@link ChatNode} and {@link ChatText} objects.
+ */
 public interface ChatSerializer {
+	/**
+	 * @param node
+	 * @return data of {@code node}.
+	 */
 	public byte[] serialize(ChatNode node);
-	public byte[] serialize(ChatText content);
 
+	/**
+	 * @param text
+	 * @return data of {@code text}.
+	 */
+	public byte[] serialize(ChatText text);
+
+	/**
+	 * Returns a object from the {@code data}.
+	 * 
+	 * @param data
+	 * @return a {@link ChatNode} or {@link ChatText} from the {@code data}.
+	 */
 	public Object deserialize(byte[] data);
 }
