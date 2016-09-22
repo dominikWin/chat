503bd98e2bc5070060ea9967c1a7050dc721f944
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:01:44 -0400

    Add doc to ChatHash

--- src/chat/interfaces/ChatHash.java
+++ src/chat/interfaces/ChatHash.java
@@ -1,13 +1,29 @@
 package chat.interfaces;
 
+/**
+ * Interface for a generic hash type.
+ */
 public interface ChatHash {
-	
+
+	/**
+	 * @return a lowercase base 16 representation of the hash.
+	 */
 	public String toString();
-	
+
+	/**
+	 * @return {@code true} if the hash is null.
+	 */
 	public boolean isNull();
-	
+
+	/**
+	 * @param other
+	 * @return {@code true} if this and {@code other} have the same hash value.
+	 */
 	public boolean equals(ChatHash other);
-	
+
+	/**
+	 * @return {@code byte[]} representation of hash.
+	 */
 	public byte[] getHash();
-	
+
 }
