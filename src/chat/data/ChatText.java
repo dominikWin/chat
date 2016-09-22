8bae37b0b69ac22a439244f2ce9653cd95b979d2
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:12:56 -0400

    Add doc to ChatText

--- src/chat/data/ChatText.java
+++ src/chat/data/ChatText.java
@@ -1,9 +1,16 @@
 package chat.data;
 
+/**
+ * Class that represents text object. Stores content of message.
+ */
 public class ChatText {
 	
 	private String content;
 	
+	/**
+	 * Creates ChatText with content.
+	 * @param content
+	 */
 	public ChatText(String content) {
 		this.content = content;
 	}
