01ba3f5ea75fd7bb256a8a18d1ea6242ba16a8b4
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:10:30 -0400

    Add doc to ChatNode

--- src/chat/data/ChatNode.java
+++ src/chat/data/ChatNode.java
@@ -1,28 +1,47 @@
 package chat.data;
 
 import chat.HashSHA1;
-import chat.ServerSettings;
-import chat.UNIXTime;
 import chat.interfaces.ChatHash;
 
+/**
+ * Class that represents a node object. Stores author, time, parent (hash), and
+ * text (hash).
+ */
 public class ChatNode {
-	
+
 	public static final String KEY_AUTHOR = "author";
 	public static final String KEY_TIME = "time";
 	public static final String KEY_TEXT = "text";
 	public static final String KEY_PARENT = "parent";
-	
+
 	private String author;
 	private long time;
 	private ChatHash text, parent;
-	
+
+	/**
+	 * Creates a node given parameters.
+	 * 
+	 * @param author
+	 * @param time
+	 * @param text
+	 * @param parent
+	 */
 	public ChatNode(String author, long time, ChatHash text, ChatHash parent) {
 		this.author = author;
 		this.time = time;
 		this.text = text;
 		this.parent = parent;
 	}
-	
+
+	/**
+	 * Create a node after creating {@link ChatHash} from {@code text} and
+	 * {@code parent}.
+	 * 
+	 * @param author
+	 * @param time
+	 * @param text
+	 * @param parent
+	 */
 	public ChatNode(String author, long time, String text, String parent) {
 		this.author = author;
 		this.time = time;
@@ -33,22 +52,17 @@ public class ChatNode {
 	public String getAuthor() {
 		return author;
 	}
-	
+
 	public long getTime() {
 		return time;
 	}
-	
+
 	public ChatHash getTextHash() {
 		return text;
 	}
-	
+
 	public ChatHash getParentHash() {
 		return parent;
 	}
-	
-//	public static ChatNode getInitialNode() {
-//		ChatNode node = new ChatNode(ServerSettings.SYSTEM_NICK, UNIXTime.getUNIXTime(), (Ch));
-//		return node;
-//	}
 
 }
