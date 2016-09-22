61eeeea7b4b0e02d38dd7ddd31631e322778a10d
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:11:39 -0400

    Add doc to ChatRef

--- src/chat/data/ChatRef.java
+++ src/chat/data/ChatRef.java
@@ -2,10 +2,18 @@ package chat.data;
 
 import chat.interfaces.ChatHash;
 
+/**
+ * Class that represents a ref object. Stores name and head (hash).
+ */
 public class ChatRef {
 	private String name;
 	private ChatHash head;
 	
+	/**
+	 * Creates ChatRef from parameters.
+	 * @param name
+	 * @param head
+	 */
 	public ChatRef(String name, ChatHash head) {
 		this.name = name;
 		this.head = head;
