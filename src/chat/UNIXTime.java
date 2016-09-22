8feb4b60529d3983d3d95c8bca581baef8dad816
Dominik Winecki <dominikwinecki@gmail.com>
2016-09-21 20:15:51 -0400

    Add doc to UNIXTime

--- src/chat/UNIXTime.java
+++ src/chat/UNIXTime.java
@@ -1,7 +1,13 @@
 package chat;
 
+/**
+ * Class with static method to return UNIX time as {@code long}.
+ */
 public class UNIXTime {
-	
+
+	/**
+	 * @return UNIX time.
+	 */
 	public static long getUNIXTime() {
 		return (long) ((long) System.currentTimeMillis() / (long) 1000);
 	}
